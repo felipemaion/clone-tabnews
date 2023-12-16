e50faf01dc87ee0c8f3676958e4ee286ee6fd813
Felipe Maion <felipe.maion@gmail.com>
2023-12-16 04:16:23 +0000

    Improved `status` response and test`

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,27 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
-async function query(queryObject){
+async function query(queryObject) {
   const client = new Client({
-    host:process.env.POSTGRES_HOST,
-    port:process.env.POSTGRES_PORT,
-    user:process.env.POSTGRES_USER,
-    database:process.env.POSTGRES_DB,
-    password:process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    idleTimeoutMillis: 100,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
-}
 
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    // Trate o erro aqui ou lance novamente para lidar em um nível superior
+    throw new Error(`Erro ao executar a consulta: ${error.message}`);
+  } finally {
+    await client.end(); // Garante que a conexão seja sempre encerrada
+  }
+}
 
 export default {
   query: query,
-}
+};
